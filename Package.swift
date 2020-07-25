754ac284d000aa6f2cf90c855c7604657de2b7b0
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-07-25 17:59:26 +0300

    Specify supported platforms in Swift PM config
    (cherry picked from commit 6393e59d02f53ab3b7ac89845b9eb827a6ebac42)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
   name: "Web3swift",
+  platforms: [
+      .iOS(.v9),
+      .tvOS(.v9),
+      .macOS(.v10_11),
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "web3swift", targets: ["web3swift"]),
