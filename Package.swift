5089d305b824c9a89e90c3c87c43cd9c4966725f
Andrew Podkovyrin <podkovyrin@gmail.com>
2021-09-17 14:17:28 +0300

    Update package

--- Package.swift
+++ Package.swift
@@ -5,24 +5,27 @@ import PackageDescription
 let package = Package(
     name: "W3SKeystore",
     platforms: [
-        .macOS(.v10_12),
+        .iOS(.v12),
+        .macOS(.v10_13),
     ],
     products: [
         .library(name: "W3SKeystore", targets: ["W3SKeystore"]),
     ],
     dependencies: [
-        .package(name: "Web3", url: "https://github.com/1inch/Web3.swift.git", from: "0.6.0"),
+        .package(name: "Web3", url: "https://github.com/1inch/Web3.swift.git", from: "0.6.11"),
     ],
     targets: [
         .target(
             name: "W3SKeystore",
             dependencies: [
                 .product(name: "Web3", package: "Web3"),
-            ]),
+            ]
+        ),
         .testTarget(
             name: "W3SKeystoreTests",
             dependencies: [
                 .target(name: "W3SKeystore"),
-            ]),
+            ]
+        ),
     ]
 )
