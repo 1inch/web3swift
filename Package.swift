623a416ef53943384ccda8860c6be0cb3bdcf097
Andrei Podkovyrin <podkovyrin@gmail.com>
2024-08-21 00:03:54 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "W3SKeystore",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
+        .iOS(.v16),
+        .macOS(.v13),
     ],
     products: [
         .library(name: "W3SKeystore", targets: ["W3SKeystore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/1inch/NewWeb3.swift", from: "0.8.7-1-oi"),
+        .package(url: "https://github.com/1inch/NewWeb3.swift", from: "0.8.8-oi.7"),
     ],
     targets: [
         .target(
